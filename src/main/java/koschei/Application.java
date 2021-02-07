f4e6cd3fe54c4553466f465661d0048a8431a735
Nikaria <cogat@mail.ru>
2021-02-07 09:16:21 +0300

    1

--- src/main/java/koschei/Application.java
+++ src/main/java/koschei/Application.java
@@ -12,6 +12,5 @@ public class Application {
                 applicationContext.getBean(KoscheiTheDeathless.class);
         System.out.println(koscheiTheDeathless.getRulesByDeth());
 
-        System.out.println();
     }
 }
